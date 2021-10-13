61b472b01718216eaf57793faa881c654e62ab97
Attila <attila.ferenc.80@gmail.com>
2021-10-13 20:22:19 +0200

    classstructureattributes Song

---
+++ training-solutions/src/main/java/classstructureattributes/SongMain.java
@@ -0,0 +1,27 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class SongMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Song song = new Song();
+
+        System.out.println("Which band plays your favourite song?");
+        song.band = scanner.nextLine();
+
+        System.out.println("What's the title of that song?");
+        song.title = scanner.nextLine();
+
+        System.out.println("How long is that song (in minutes)?");
+        song.length = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println(song.band + " - " + song.title + " (" + song.length + " minutes)");
+
+
+    }
+
+}
