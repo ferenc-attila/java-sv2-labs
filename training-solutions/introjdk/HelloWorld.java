99daed455dc8a5a1fc0e07907cde38a486f53a34
Attila <attila.ferenc.80@gmail.com>
2021-10-13 05:57:04 +0200

    introjdk solution

---
+++ training-solutions/introjdk/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	// A class neve a fájlnév, a package neve a fájlt tartalmazó mappa neve.
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
