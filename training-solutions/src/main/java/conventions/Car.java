f36fa25a3aefda16c77d817bacd0b7aa98349afb
Attila <attila.ferenc.80@gmail.com>
2021-10-17 07:36:39 +0200

    conventions.Car formatted

--- training-solutions/src/main/java/conventions/Car.java
+++ training-solutions/src/main/java/conventions/Car.java
@@ -2,41 +2,51 @@ package conventions;
 
 public class Car {
 
-    public void setCarType(String carType) {this.CarType = carType;}
-
-    private String CarType; private String engineType;
+    private String carType;
+    private String engineType;
     private int doors;
+    private int persons;
+
+    public Car(String carType, String engineType, int doors, int persons) {
+        this.carType = carType;
+        this.engineType = engineType;
+        this.doors = doors;
+        this.persons = persons;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public String GetEngineType() {
+        return engineType;
+    }
+
     public int getPersons() {
         return persons;
     }
-    public Car(String carType, String engineType, int DOORs, int persons)
 
-
-
-    {
-        this.CarType = carType;
-        this.engineType = engineType;
-        this.doors = DOORs;
-        this.persons = persons;
-    }
-    private int persons;
-    public String getCarType() {
-        return CarType;
-    }public String GetEngineType() {
-        return engineType; }public int getdoors() {
+    public int getdoors() {
         return doors;
     }
 
-
-
-
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
 
     public void setEngineType(String engineType) {
-        this.engineType = engineType;}
+        this.engineType = engineType;
+    }
 
     public void SetDoors(int doors) {
-        this.doors = doors;}public void SetPersonS(int persons) {
-        this.persons = persons;}
-    public void addModelName(String MODELName) {
-        this.CarType = CarType + " " + MODELName;
+        this.doors = doors;
     }
+
+    public void SetPersonS(int persons) {
+        this.persons = persons;
+    }
+
+    public void addModelName(String MODELName) {
+        this.carType = carType + " " + MODELName;
+    }
+}
