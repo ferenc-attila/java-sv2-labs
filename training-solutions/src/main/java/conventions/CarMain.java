c5df725e12e277a71a0a89befd4bc3c7e00753eb
Attila <attila.ferenc.80@gmail.com>
2021-10-17 08:03:38 +0200

    CarMAin created and new Car construction added

---
+++ training-solutions/src/main/java/conventions/CarMain.java
@@ -0,0 +1,24 @@
+package conventions;
+
+import java.util.Scanner;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the manufacturer of your car:");
+        String type = scanner.nextLine();
+        System.out.println("Enter the engine type of your car:");
+        String engine = scanner.nextLine();
+        System.out.println("Enter the number of doors on your car:");
+        int doors = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(("Enter the number of persons your car can carry:"));
+        int persons = scanner.nextInt();
+        scanner.nextLine();
+
+        Car car = new Car(type, engine, doors, persons);
+    }
+}
