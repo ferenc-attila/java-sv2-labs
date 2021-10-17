9fac7f5a60449c4a842efd79fdd97e072bbc648c
Attila <attila.ferenc.80@gmail.com>
2021-10-17 10:57:28 +0200

    DistanceMain created, excercise solved

--- training-solutions/src/main/java/localvariables/DistanceMain.java
+++ training-solutions/src/main/java/localvariables/DistanceMain.java
@@ -1,2 +1,17 @@
-package localvariables;public class DistanceMain {
+package localvariables;
+
+public class DistanceMain {
+
+    public static void main(String[] args) {
+
+    Distance distance = new Distance(5.547, false);
+        System.out.println("The distance is " + distance.getDistanceInKm() + " km and " + distance.isExact() + ".");
+
+        int distanceInteger = (int) distance.getDistanceInKm();
+
+        System.out.println(distanceInteger);
+    }
+
+
+
 }
