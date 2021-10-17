8d283a8677081bcbf92d88ce5d9d885e793366cc
Attila <attila.ferenc.80@gmail.com>
2021-10-17 10:41:12 +0200

    LocalVariablesMain.java created

---
+++ training-solutions/src/main/java/localvariables/Distance.java
@@ -0,0 +1,2 @@
+package localvariables;public class Distance {
+}
---
+++ training-solutions/src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,35 @@
+package localvariables;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+
+        boolean b = false;
+        System.out.println(b);
+
+        int a = 2;
+
+        System.out.println(a);
+
+        int i = 3;
+        int j = 4;
+        int k = i;
+
+        System.out.println("i: " + i + "; j: " + j + "; k: " + k + ";");
+
+        String s = "Hello World!";
+        String t = s;
+
+        System.out.println("s: " + s + "; t: " + t + ";");
+
+        {
+            int x = 0;
+
+            System.out.println("x: " + x + "; a: " + a + ";");
+        }
+
+        System.out.println(a);
+
+    }
+
+}
