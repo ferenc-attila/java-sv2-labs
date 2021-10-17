9352665151663a578615203a8bc4438783c29809
Attila <attila.ferenc.80@gmail.com>
2021-10-17 10:44:23 +0200

    Distance.java created

--- training-solutions/src/main/java/localvariables/Distance.java
+++ training-solutions/src/main/java/localvariables/Distance.java
@@ -1,2 +1,20 @@
-package localvariables;public class Distance {
+package localvariables;
+
+public class Distance {
+
+    double distanceInKm;
+    boolean exact;
+
+    public Distance(double distanceInKm, boolean exact) {
+        this.distanceInKm = distanceInKm;
+        this.exact = exact;
+    }
+
+    public double getDistanceInKm() {
+        return distanceInKm;
+    }
+
+    public boolean isExact() {
+        return exact;
+    }
 }
