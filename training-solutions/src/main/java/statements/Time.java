f528dc0a608f795e28cdb246f47c2f81228325f3
Attila <attila.ferenc.80@gmail.com>
2021-10-17 18:19:27 +0200

    statements.Time added

---
+++ training-solutions/src/main/java/statements/Time.java
@@ -0,0 +1,45 @@
+package statements;
+
+public class Time {
+
+    int hours;
+    int minutes;
+    int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public int getInMinutes() {
+        int inMinutes = (getHours() * 60) + getMinutes();
+        return inMinutes;
+    }
+
+    public int getInSeconds() {
+        int inSeconds = (getHours() * 3600) + getInMinutes() * 60 + getSeconds();
+        return inSeconds;
+    }
+
+    public boolean earlierThan (Time firstTime, Time secondTime) {
+        boolean earlierThan = firstTime.getInSeconds() < secondTime.getInSeconds();
+        return earlierThan;
+    }
+
+    public String toString () {
+        String timeString = this.hours + ":" + this.minutes + ":" + this.seconds;
+    }
+}
