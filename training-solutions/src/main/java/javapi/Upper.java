d7c27b343d8f3bf2b841d30d51d0a2f596ffb4f7
Attila <attila.ferenc.80@gmail.com>
2021-10-18 09:35:36 +0200

    Upper solved

---
+++ training-solutions/src/main/java/javapi/Upper.java
@@ -0,0 +1,14 @@
+package javapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+    String hello = "Hello Árvíztűrő Tükörfúrógép!";
+
+    String upperHello = hello.toUpperCase();
+
+        System.out.println(upperHello);
+    }
+
+}
