23aabb8b0929ed1315f5af6fff69fc3c65488028
Attila <attila.ferenc.80@gmail.com>
2021-10-18 20:46:59 +0200

    stringtype.registration.Registration constructors and getters added

--- training-solutions/src/main/java/stringtype/registration/Registration.java
+++ training-solutions/src/main/java/stringtype/registration/Registration.java
@@ -7,7 +7,27 @@ public class Registration {
     private String passwordCheck;
     private String email;
 
+   public void Registration (String name, String password, String passwordCheck, String email) {
+       this.name = name;
+       this.password = password;
+       this.passwordCheck = passwordCheck;
+       this.email = email;
+   }
+
+   public String getName() {
+       return name;
+   }
+
+   public String getPassword() {
+       return password;
+   }
+
+   public String getPasswordCheck() {
+       return passwordCheck;
+   }
+
+   public String getEmail () {
+       return email;
+   }
    
-
-
 }
