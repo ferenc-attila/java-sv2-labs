e803b2566b04a8678669f0c4305ffa2c3c846c2e
Attila <attila.ferenc.80@gmail.com>
2021-10-18 08:54:34 +0200

    complete name excercise solved

---
+++ training-solutions/src/main/java/packages/namemain/NameMain.java
@@ -0,0 +1,32 @@
+package packages.namemain;
+
+import packages.firstname.FirstName;
+import packages.lastname.LastName;
+import packages.prefix.Prefix;
+
+import java.util.Scanner;
+
+public class NameMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name's prefix:");
+        String prefix = scanner.nextLine();
+        System.out.println("Enter your first name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your last name:");
+        String lastName = scanner.nextLine();
+
+        Prefix pre = new Prefix(prefix);
+        FirstName first = new FirstName(firstName);
+        LastName last = new LastName(lastName);
+
+        String completeName = pre.getPre() + " " + first.getFirst() + " " + last.getLast();
+
+        System.out.println(completeName);
+
+    }
+
+}
--- training-solutions/src/main/java/packages/prefix/Prefix.java
+++ training-solutions/src/main/java/packages/prefix/Prefix.java
@@ -1,4 +1,15 @@
 package packages.prefix;
 
 public class Prefix {
+
+    private String pre;
+
+    public Prefix(String pre) {
+        this.pre = pre;
+    }
+
+    public String getPre() {
+        return pre;
+    }
+
 }
