cd64b863fccf99e7431696f8ee974f3b1c488139
Attila <attila.ferenc.80@gmail.com>
2021-10-18 19:41:18 +0200

    stringtype.StringTypeMain solved

---
+++ training-solutions/src/main/java/stringtype/StringTypeMain.java
@@ -0,0 +1,36 @@
+package stringtype;
+
+public class StringTypeMain {
+
+    public static void main(String[] args) {
+
+        String prefix = "Hello ";
+        System.out.println(prefix);
+
+        String name = "John Doe";
+        System.out.println(name);
+
+        String message = prefix + name;
+        System.out.println(message);
+
+        message = message + 444;
+        System.out.println(message);
+
+        boolean b = message.equals("Hello John Doe");
+        System.out.println(b);
+
+        boolean c = message.equals("Hello John Doe444");
+        System.out.println(c);
+
+        String emptyString = ""+"";
+        System.out.println(emptyString);
+        System.out.println(emptyString.length());
+
+        String abc = "Abcde";
+
+        System.out.println(abc.length());
+        System.out.println(abc.substring(0,1) + ", " + abc.substring(2,3));
+        System.out.println(abc.substring(0,3));
+
+    }
+}
