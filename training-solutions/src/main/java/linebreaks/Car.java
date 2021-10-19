808776df6bf7558938b70d3183463afde83a9673
ferenc-attila <attila.ferenc.80@gmail.com>
2021-10-19 11:55:41 +0200

    linebreaks.Car and CarMain solved

---
+++ training-solutions/src/main/java/linebreaks/Car.java
@@ -0,0 +1,9 @@
+package linebreaks;
+
+public class Car {
+
+    public void getBrandAndTypeInSeparateLines (String brand, String type) {
+        System.out.print(brand + "\n" + type + "\n");
+    }
+
+}
---
+++ training-solutions/src/main/java/linebreaks/CarMain.java
@@ -0,0 +1,15 @@
+package linebreaks;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        String brand = "Opel";
+        String type = "Astra";
+
+        car.getBrandAndTypeInSeparateLines(brand, type);
+
+    }
+}
