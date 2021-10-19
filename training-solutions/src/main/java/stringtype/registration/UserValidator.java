bb287846d3e096e5067705f8ca90b7626deea1a9
Attila <attila.ferenc.80@gmail.com>
2021-10-19 09:21:14 +0200

    stringtype UserValidator tested and isValidEmail method updated, SonarLint advices implememnted

--- training-solutions/src/main/java/stringtype/registration/UserValidator.java
+++ training-solutions/src/main/java/stringtype/registration/UserValidator.java
@@ -3,23 +3,20 @@ package stringtype.registration;
 public class UserValidator {
 
     public boolean isValidUsername(String username) {
-        boolean isValidUsername = username.equals("");
-        return !isValidUsername;
+        return !username.equals("");
     }
 
     public boolean isValidPassword(String password1, String password2) {
-        boolean isValidPassword = password1.equals(password2) && password1.length() >= 8;
-        return  isValidPassword;
+        return password1.equals(password2) && password1.length() >= 8;
     }
 
     public boolean isValidEmail(String email) {
 
-        int indexOfAtSign = email.indexOf("@", 1);
-        int indexOfDot = email.indexOf(".");
         int emailStringLength = email.length();
+        int indexOfAtSign = email.indexOf("@", 1);
+        int indexOfDot = email.indexOf(".", indexOfAtSign+2);
 
-        boolean isValidEmail = indexOfAtSign != -1 && indexOfDot > indexOfAtSign+1 && indexOfDot < emailStringLength-2;
-        return isValidEmail;
+        return indexOfAtSign != -1 && indexOfDot != -1 && indexOfDot < emailStringLength-1;
     }
 
 }
