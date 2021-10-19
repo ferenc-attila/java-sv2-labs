6cbfd54ab540f3b938ba8e920b42317f2537884f
Attila <attila.ferenc.80@gmail.com>
2021-10-19 22:27:09 +0200

    introdate Employee created

--- training-solutions/src/main/java/introdate/Employee.java
+++ training-solutions/src/main/java/introdate/Employee.java
@@ -1,2 +1,34 @@
-package introdate;public class Employee {
+package introdate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Employee {
+
+    private String name;
+    private LocalDate dateOfBirth;
+    private LocalDateTime beginEmployment;
+
+    public Employee (String name, LocalDate dateOfBirth, LocalDateTime beginEmployment) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.beginEmployment = beginEmployment;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getBeginEmployment() {
+        return beginEmployment;
+    }
+
+    public void setName(name) {
+        this.name = name;
+    }
+
 }
