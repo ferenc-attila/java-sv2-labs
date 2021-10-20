f4156dcb9f48c32fb3ffbc64e7664ef115db517c
ferenc-attila <attila.ferenc.80@gmail.com>
2021-10-20 20:25:50 +0200

    introcontrol.Sum solved

--- training-solutions/src/main/java/introcontrol/Sum.java
+++ training-solutions/src/main/java/introcontrol/Sum.java
@@ -1,2 +1,20 @@
-package introcontrol;public class Sum {
+package introcontrol;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter five numbers");
+        double sum = 0;
+        for (int i = 0; i<5; i++) {
+            sum = sum + scanner.nextDouble();
+            scanner.nextLine();
+        }
+
+        System.out.println("Sum of your numbers: " + sum);
+    }
 }
