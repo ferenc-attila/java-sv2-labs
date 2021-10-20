47049cf940b71ef7b3edee50ccafd54d9caeb1e1
ferenc-attila <attila.ferenc.80@gmail.com>
2021-10-20 20:26:20 +0200

    introcontrol.UserMenu solved

--- training-solutions/src/main/java/introcontrol/UserMenu.java
+++ training-solutions/src/main/java/introcontrol/UserMenu.java
@@ -1,2 +1,28 @@
-package introcontrol;public class UserMenu {
+package introcontrol;
+
+import java.util.Scanner;
+
+public class UserMenu {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Listing users");
+        System.out.println("2. Add new users");
+        System.out.println("Other: Exit");
+        System.out.println("Select an option");
+
+        int option = scanner.nextInt();
+
+        if (option == 1){
+                System.out.println("Listing users");
+        } else {
+            if (option == 2) {
+                System.out.println("Add new users");
+            } else {
+            return;
+            }
+        }
+    }
 }
