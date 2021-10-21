222c44d092b148a034799d7c719464d2e1028ba5
Attila <attila.ferenc.80@gmail.com>
2021-10-21 23:33:04 +0200

    arrayofarrays.Rectangle solved

---
+++ training-solutions/src/main/java/arrayofarrays/Rectangle.java
@@ -0,0 +1,23 @@
+package arrayofarrays;
+
+public class Rectangle {
+
+    public int [][] rectangularMatrix (int size) {
+        int [][] rectangle = new int [size][size];
+        for (int i = 0; i < size; i++) {
+            rectangle[i] = new int[size];
+            for (int j = 0; j < size; j++) {
+                rectangle[i][j] = i;
+            }
+        }
+        return rectangle;
+    }
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        ArrayOfArrays arrayOfArray = new ArrayOfArrays();
+
+        arrayOfArray.printArrayOfArrays(rectangle.rectangularMatrix(5));
+    }
+}
--- training-solutions/src/main/java/arrayofarrays/Triangular.java
+++ training-solutions/src/main/java/arrayofarrays/Triangular.java
@@ -3,9 +3,7 @@ package arrayofarrays;
 public class Triangular {
 
     public int[][] triangularMatrix(int size) {
-
         int[][] triangular = new int[size][];
-
         for (int i = 0; i < size; i++) {
             for (int j = 0; j<=i; j++) {
                 triangular[i] = new int[j+1];
