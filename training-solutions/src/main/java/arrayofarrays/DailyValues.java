b9a513fe815de37a1b009c1864738ef27e471ff3
Attila <attila.ferenc.80@gmail.com>
2021-10-21 23:47:42 +0200

    arrayofarrays.dailyValues solved

---
+++ training-solutions/src/main/java/arrayofarrays/DailyValues.java
@@ -0,0 +1,22 @@
+package arrayofarrays;
+
+public class DailyValues {
+
+    int[][] getValues() {
+        int[][] dailyValues = new int[12][];
+        int[] daysInMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int i = 0; i<12; i++) {
+            dailyValues[i] = new int[daysInMonths[i]];
+        }
+        return dailyValues;
+    }
+
+    public static void main(String[] args) {
+        DailyValues dailyValues = new DailyValues();
+        ArrayOfArrays arrayOfArray = new ArrayOfArrays();
+
+        arrayOfArray.printArrayOfArrays(dailyValues.getValues());
+    }
+
+}
