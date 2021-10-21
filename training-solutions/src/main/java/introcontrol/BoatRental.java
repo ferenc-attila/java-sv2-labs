6f82a11372d1ed4d4ebd5ac80cc93069bb8a68bf
Attila <attila.ferenc.80@gmail.com>
2021-10-21 11:07:10 +0200

    BoatRental simplified

--- training-solutions/src/main/java/introcontrol/BoatRental.java
+++ training-solutions/src/main/java/introcontrol/BoatRental.java
@@ -16,39 +16,33 @@ public class BoatRental {
             linedUp = numberOfGuests - 10;
             numberOfGuests = 10;
         }
-
         if (numberOfGuests <= 0) {
             System.out.println("Not a valid number! Enter positive integer!");
-        } else {
-            if (numberOfGuests <= 2) {
-                System.out.println("The boat for 2 ran out, there are still 8 free seats.");
-            } else {
-                if (numberOfGuests == 3) {
-                    System.out.println("The boat for 3 ran out, there are still 7 free seats.");
-                } else {
-                    if (numberOfGuests <= 5) {
-                        System.out.println("The boat for 5 ran out, there are still 5 free seats.");
-                    } else {
-                        if (numberOfGuests <=7) {
-                            System.out.println("The boat for 5 and the boat for 2 ran out, there are still 3 free seats.");
-                        } else {
-                            if (numberOfGuests == 8) {
-                                System.out.println("The boat for 5 and the boat for 3 ran out, there are still 2 free seats.");
-                            } else {
-                                System.out.println("All of the boats ran out, there isn't any free seat.");
-                            }
-                        }
-                    }
-                }
-            }
         }
-
+        if (numberOfGuests <= 2) {
+            System.out.println("The boat for 2 ran out, there are still 8 free seats.");
+        }
+        if (numberOfGuests == 3) {
+            System.out.println("The boat for 3 ran out, there are still 7 free seats.");
+        }
+        if (numberOfGuests <= 5) {
+            System.out.println("The boat for 5 ran out, there are still 5 free seats.");
+        }
+        if (numberOfGuests <= 7) {
+            System.out.println("The boat for 5 and the boat for 2 ran out, there are still 3 free seats.");
+        }
+        if (numberOfGuests == 8) {
+            System.out.println("The boat for 5 and the boat for 3 ran out, there are still 2 free seats.");
+        }
+        System.out.println("All of the boats ran out, there isn't any free seat.");
+        
         if (linedUp == 1) {
             System.out.println("There is a person waiting on the shore");
-        } else {
-            if (linedUp > 1) {
-                System.out.println("There are " + linedUp + " persons waiting on the shore.");
-            }
+        }
+        if (linedUp > 1) {
+            System.out.println("There are " + linedUp + " persons waiting on the shore.");
         }
     }
 }
+
+
