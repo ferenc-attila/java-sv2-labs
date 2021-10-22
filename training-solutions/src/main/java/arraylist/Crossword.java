d20e1af0e9aa75aba7a7cc65cbd26e27b6abb19e
Attila <attila.ferenc.80@gmail.com>
2021-10-22 15:07:07 +0200

    arraylist.Crossword solved

---
+++ training-solutions/src/main/java/arraylist/Crossword.java
@@ -0,0 +1,17 @@
+package arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Crossword {
+
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("KULCS", "KÁLYHA", "LÓ", "AJTÓ", "CSERESZNYEFA", "TEJ", "FELHŐ", "CIPŐ", "MOSODA", "KÁVÉTEJSZÍN", "CITERA", "BABA");
+        for (String word: words){
+            if (word.length() == 6) {
+                System.out.println(word);
+            }
+        }
+    }
+}
