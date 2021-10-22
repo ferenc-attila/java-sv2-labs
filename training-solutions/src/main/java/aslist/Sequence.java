82421ab609d3063b15aa6f8d0baf29e76b6b6233
Attila <attila.ferenc.80@gmail.com>
2021-10-22 18:08:30 +0200

    aslist.Sequence solved

---
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -0,0 +1,19 @@
+package aslist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+
+        List<Integer> elements = new ArrayList<>(Arrays.asList(5, 7));
+        int numberOfNewElements = 5;
+        for (int i = 2; i-2 < numberOfNewElements; i++) {
+            elements.add(elements.get(i - 2) * elements.get(i - 1));
+        }
+        System.out.println(elements.size());
+        System.out.println(elements);
+    }
+}
