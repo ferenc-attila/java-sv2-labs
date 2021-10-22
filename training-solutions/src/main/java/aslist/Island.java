7409458ae80382101346bcd993e791c144b65444
Attila <attila.ferenc.80@gmail.com>
2021-10-22 18:09:34 +0200

    aslist.Island List name refactored

--- training-solutions/src/main/java/aslist/Island.java
+++ training-solutions/src/main/java/aslist/Island.java
@@ -8,11 +8,11 @@ public class Island {
     public static void main(String[] args) {
 
         System.out.print("My original list: ");
-        List<String> elements = Arrays.asList("Lord of the Rings book", "axe", "hammock");
-        System.out.println(elements);
+        List<String> importantThings = Arrays.asList("Lord of the Rings book", "axe", "hammock");
+        System.out.println(importantThings);
 
         System.out.print("My modified list: ");
-        elements.set(1, "hand saw");
-        System.out.println(elements);
+        importantThings.set(1, "hand saw");
+        System.out.println(importantThings);
     }
 }
