43783b0f07da403bf2fa881c479ac5bed7517421
Attila <attila.ferenc.80@gmail.com>
2021-10-22 15:10:59 +0200

    arraylist.Second solved

---
+++ training-solutions/src/main/java/arraylist/Second.java
@@ -0,0 +1,16 @@
+package arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Second {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(3, 9, 42, 67, 4, 0, 76, 14, 84, 35, 92);
+
+        for (int i=0; i<numbers.size(); i+=2){
+            System.out.println(numbers.get(i));
+        }
+    }
+}
