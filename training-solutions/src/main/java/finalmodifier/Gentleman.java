b409243168ebb47d27dc2d121e9f16a586c92be4
Attila <attila.ferenc.80@gmail.com>
2021-10-22 22:39:19 +0200

    finalmodifier.Gentleman solved

---
+++ training-solutions/src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,10 @@
+package finalmodifier;
+
+public class Gentleman {
+
+    public static final String MESSAGE_PREFIX = "Hello ";
+
+    public static String sayHello(String name) {
+        return (MESSAGE_PREFIX + name + "!");
+    }
+}
---
+++ training-solutions/src/main/java/finalmodifier/GentlemanMain.java
@@ -0,0 +1,8 @@
+package finalmodifier;
+
+public class GentlemanMain {
+
+    public static void main(String[] args) {
+        System.out.println(Gentleman.sayHello("Jack"));
+    }
+}
