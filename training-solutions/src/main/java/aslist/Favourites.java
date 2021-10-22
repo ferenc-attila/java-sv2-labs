3328c3246da4edbcdc6c2397b1ec34977dd707b6
Attila <attila.ferenc.80@gmail.com>
2021-10-22 17:51:18 +0200

    aslist.Favourites solved

--- training-solutions/src/main/java/aslist/Favourites.java
+++ training-solutions/src/main/java/aslist/Favourites.java
@@ -1,2 +1,34 @@
-package aslist;public class Favourites {
+package aslist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Favourites {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> favouriteThings = new ArrayList<>();
+
+        System.out.println("Enter your favourite animal:");
+        String thing = scanner.nextLine();
+        favouriteThings.add(thing);
+        System.out.println("Enter your favourite color:");
+        thing = scanner.nextLine();
+        favouriteThings.add(thing);
+        System.out.println("Enter your favourite movie:");
+        thing = scanner.nextLine();
+        favouriteThings.add(thing);
+        System.out.println("Enter your favourite actor:");
+        thing = scanner.nextLine();
+        favouriteThings.add(thing);
+        System.out.println("Enter your favourite city:");
+        thing = scanner.nextLine();
+        favouriteThings.add(thing);
+
+        System.out.println("Your list contains " + favouriteThings.size() + " element");
+        System.out.println("The list of your favourite things contains the elements below:");
+        System.out.println(favouriteThings);
+    }
 }
