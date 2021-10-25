5084d04baf1c20b7a80dcd60e2cb648f93c0e3e2
Attila <attila.ferenc.80@gmail.com>
2021-10-25 11:13:59 +0200

    references.parameters Person, ReferencesMain solved

--- training-solutions/src/main/java/references/parameters/ReferencesMain.java
+++ training-solutions/src/main/java/references/parameters/ReferencesMain.java
@@ -1,2 +1,20 @@
-package references.parameters;public class ReferencesMain {
+package references.parameters;
+
+public class ReferencesMain {
+
+    public static void main(String[] args) {
+
+        Person personOne;
+        Person personTwo;
+
+        Person personThree = new Person("John Doe", 45);
+        personOne = personThree;
+        personTwo = personOne;
+
+        personTwo.setName("Jane Doe");
+
+        System.out.println(personOne.getName());
+        System.out.println(personTwo.getName());
+        System.out.println(personThree.getName());
+    }
 }
