792d3bd090bf1b1bc824edb9435806f94e87c115
Attila <attila.ferenc.80@gmail.com>
2021-10-25 20:09:20 +0200

    references dogs solved

--- training-solutions/src/main/java/references/dogs/Dog.java
+++ training-solutions/src/main/java/references/dogs/Dog.java
@@ -1,2 +1,45 @@
-package references.dogs;public class Dog {
+package references.dogs;
+
+public class Dog {
+
+    private String name;
+    private int age;
+    private String colour;
+
+    public Dog(String name, int age, String colour) {
+        this.name = name;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + ";\n" +
+               "age: " + age + ";\n" +
+               "colour: " + colour + ";\n";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
--- training-solutions/src/main/java/references/dogs/Kennel.java
+++ training-solutions/src/main/java/references/dogs/Kennel.java
@@ -1,2 +1,64 @@
-package references.dogs;public class Kennel {
+package references.dogs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kennel {
+
+    public static void main(String[] args) {
+
+        List<Dog> dogs = new ArrayList<>();
+
+        Dog dog1 = new Dog("Csöpi", 5, "zsemle");
+        Dog dog2 = new Dog("Bogáncs", 7, "fekete");
+        Dog dog3 = new Dog("Cserge", 6, "fehér");
+
+        dogs.add(dog1);
+        dogs.add(dog2);
+        dogs.add(dog3);
+//        System.out.println(dogs);
+
+        Dog dog4 = dogs.get(1);
+        dogs.add(dog4);
+//        System.out.println(dogs);
+
+        Dog dog5 = new Dog("Hektor", 8, "szürke");
+//        System.out.println(dog5);
+
+        dog5 = dogs.get(1);
+        dogs.add(dog5);
+//        System.out.println(dogs);
+
+        Dog dog6 = dog4;
+        dog6.setColour("szürke");
+//        dogs.add(dog6);
+//        System.out.println(dogs);
+
+        List<Dog> dogs2 = new ArrayList<>();
+        dogs2 = dogs;
+
+        Dog dog7 = dog4;
+        dog7.setColour("csokoládé");
+//        System.out.println(dogs2);
+
+        dog2.setColour("barna");
+//        System.out.println(dogs2);
+
+        dog2 = null;
+//        System.out.println(dogs2);
+
+        Dog dog8 = dogs.get(1);
+        dog8 = null;
+//        System.out.println(dogs2);
+
+        Dog dog9 = dogs.get(1);
+        dog9.setColour("fekete");
+//        System.out.println(dogs2);
+
+//        System.out.println(dog2);
+//        dog2.setColour("fehér");
+        System.out.println(dog2);
+        System.out.println(dogs);
+    }
 }
