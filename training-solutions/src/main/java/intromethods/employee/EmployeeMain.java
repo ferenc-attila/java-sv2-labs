2a4020b089c0c9877c2489274c570041a66a783f
Attila <attila.ferenc.80@gmail.com>
2021-10-25 07:22:22 +0200

    introconstructors Employee solved

---
+++ training-solutions/src/main/java/intromethods/employee/EmployeeMain.java
@@ -0,0 +1,14 @@
+package intromethods.employee;
+
+public class EmployeeMain {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Jhon Doe", 2012, 250_000);
+        System.out.println(employee.toString());
+
+        employee.setName("John Doe");
+        employee.raiseSalary(50_000);
+        System.out.println(employee.toString());
+    }
+}
