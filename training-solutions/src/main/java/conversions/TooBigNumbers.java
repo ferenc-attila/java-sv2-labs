1ff4cd2557dafdbd56cec4ca6c12d1bcc73e901c
Attila <attila.ferenc.80@gmail.com>
2021-10-30 17:41:41 +0200

    conversions.TooBigNumbers solved

---
+++ training-solutions/src/main/java/conversions/TooBigNumbers.java
@@ -0,0 +1,8 @@
+package conversions;
+
+public class TooBigNumbers {
+
+    public long getRightResults(int number) {
+        return (long) 2_147_483_647 + number;
+    }
+}
---
+++ training-solutions/src/main/java/conversions/TooBigNumbersMain.java
@@ -0,0 +1,10 @@
+package conversions;
+
+public class TooBigNumbersMain {
+
+    public static void main(String[] args) {
+
+        TooBigNumbers tooBigNumbers = new TooBigNumbers();
+        System.out.println(tooBigNumbers.getRightResults(1_000_000_000));
+    }
+}
