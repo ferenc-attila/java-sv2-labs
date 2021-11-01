07a13583e3d70e3b1319bf3a86fd5ce337f82415
Attila <attila.ferenc.80@gmail.com>
2021-11-01 12:40:21 +0100

    DayInMonth solved and Tested

--- training-solutions/src/main/java/controlselection/month/DayInMonth.java
+++ training-solutions/src/main/java/controlselection/month/DayInMonth.java
@@ -1,2 +1,37 @@
-package controlselection.month;public class DayInMonth {
+package controlselection.month;
+
+public class DayInMonth {
+
+    public int dayInMonth (int year, String nameOfMonth){
+        int dayInMonth;
+        nameOfMonth = nameOfMonth.toLowerCase();
+        switch (nameOfMonth) {
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                dayInMonth = 31;
+                break;
+            case "február":
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    dayInMonth = 29;
+                } else {
+                    dayInMonth = 28;
+                }
+                break;
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                dayInMonth = 30;
+                break;
+            default:
+                dayInMonth = 0;
+                break;
+        }
+        return dayInMonth;
+    }
 }
--- training-solutions/src/test/java/controlselection/month/DayInMonthTest.java
+++ training-solutions/src/test/java/controlselection/month/DayInMonthTest.java
@@ -1,2 +1,45 @@
-package controlselection.month;public class DayInMonthTest {
+package controlselection.month;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DayInMonthTest {
+
+    DayInMonth dayInMonth = new DayInMonth();
+
+    @Test
+    void testThirtyDays() {
+        assertEquals(30, dayInMonth.dayInMonth(2021, "ÁPRILIS"));
+    }
+
+    @Test
+    void testThirtyOneDays() {
+        assertEquals(31, dayInMonth.dayInMonth(2021, "DecEmbEr"));
+    }
+
+    @Test
+    void testFebruaryInLeapYear() {
+        assertEquals(29, dayInMonth.dayInMonth(2020, "február"));
+    }
+
+    @Test
+    void testFebruaryInLeapYearDivisibleByFourHundred() {
+        assertEquals(29, dayInMonth.dayInMonth(2000, "február"));
+    }
+
+    @Test
+    void testFebruaryInNonLeapHundredYear() {
+        assertEquals(28, dayInMonth.dayInMonth(1900, "február"));
+    }
+
+    @Test
+    void testFebruaryInNonLeapYear() {
+        assertEquals(28, dayInMonth.dayInMonth(2021, "február"));
+    }
+
+    @Test
+    void testDefaultReturn() {
+        assertEquals(0, dayInMonth.dayInMonth(2021, "January"));
+    }
 }
