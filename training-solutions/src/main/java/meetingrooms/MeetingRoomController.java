2a9310ef0a0c007fa7785e728894a328c2258c5e
Attila <attila.ferenc.80@gmail.com>
2021-11-02 21:46:33 +0100

    meetingroom.MeetingRoomController readOffice() added

--- training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
+++ training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
@@ -1,6 +1,24 @@
 package meetingrooms;
 
+import java.util.Scanner;
+
 public class MeetingRoomController {
+
+    Scanner scanner = new Scanner(System.in);
+    Office office = new Office();
+
+    public void readOffice () {
+        System.out.println("Adja meg a tárgyaló nevét:");
+        String name = scanner.nextLine();
+        System.out.println("Adja meg a tárgyaló szélességét:");
+        int width = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(("Adja meg a tárgyaló hosszúságát:"));
+        int length = scanner.nextInt();
+        scanner.nextLine();
+
+        office.addMeetingRoom(new MeetingRoom(name, length, width));
+    }
 
     public void printMenu() {
         System.out.println("1. Tárgyaló rögzítése\n" +
