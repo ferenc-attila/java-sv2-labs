b62ccef95a2c54a28c7a7c07ea2f0e6f557d5ba7
Attila <attila.ferenc.80@gmail.com>
2021-11-03 18:37:37 +0100

    meetingrooms.MeetingRoomController runMenu() updated using arrow switch

--- training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
+++ training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
@@ -43,50 +43,47 @@ public class MeetingRoomController {
         scanner.nextLine();
 
         switch (selectedMenu) {
-            case 1:
+            case 1 -> {
                 readOffice();
                 runMenu();
-                break;
-            case 2:
+            }
+            case 2 -> {
                 office.printNames();
                 runMenu();
-                break;
-            case 3:
+            }
+            case 3 -> {
                 office.printNamesReverse();
                 runMenu();
-                break;
-            case 4:
+            }
+            case 4 -> {
                 office.printEvenNames();
                 runMenu();
-                break;
-            case 5:
+            }
+            case 5 -> {
                 office.printAreas();
                 runMenu();
-                break;
-            case 6:
+            }
+            case 6 -> {
                 System.out.println("Adja meg a keresendő tárgyaló nevét:");
                 String name = scanner.nextLine();
                 office.printMeetingRoomsWithName(name);
                 runMenu();
-                break;
-            case 7:
+            }
+            case 7 -> {
                 System.out.println("Adja meg, milyen kifejezést keressek a tárgyalók nevében:");
                 String part = scanner.nextLine();
                 office.printMeetingRoomsContains(part);
                 runMenu();
-                break;
-            case 8:
+            }
+            case 8 -> {
                 System.out.println("Adja meg, mekkora területnél keressek nagyobb méretű tárgyalót:");
                 int area = scanner.nextInt();
                 scanner.nextLine();
                 office.printAreasLargerThan(area);
                 runMenu();
-                break;
-            case 9:
-                System.out.println("Köszönjük, hogy használta szoftverünket! Viszontlátásra!");
-                break;
-            default:
-                runMenu();
+            }
+            case 9 -> System.out.println("Köszönjük, hogy használta szoftverünket! Viszontlátásra!");
+            default -> runMenu();
         }
     }
 
