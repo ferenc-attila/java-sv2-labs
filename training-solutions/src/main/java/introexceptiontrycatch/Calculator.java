beb1c66899b5067c49ccf483e2252a1ff23e7190
Attila <attila.ferenc.80@gmail.com>
2021-11-18 12:31:07 +0100

    introexceptiontrycatch solved

--- training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
+++ training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
@@ -1,36 +1,60 @@
 package introexceptiontrycatch;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter an integer");
-        int firstNumber = scanner.nextInt();
+        int firstNumber = 0;
+        try {
+            firstNumber = scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Not an integer");
+            System.out.println("First integer will be 0");
+            System.out.println(inputMismatchException.getMessage());
+        }
+        scanner.nextLine();
         System.out.println("Enter another integer");
-        int secondNumber = scanner.nextInt();
+        int secondNumber = 0;
+        try {
+            secondNumber = scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Not an integer");
+            System.out.println("Second integer will be 0");
+            System.out.println(inputMismatchException.getMessage());
+        }
+
+        scanner.nextLine();
         System.out.println("Which operation do you want to use? ('+', '-', '/', '*') Enter an operator");
         String operator = scanner.nextLine();
 
+        int result;
+        try {
         switch (operator) {
             case "+":
-                System.out.println(firstNumber + secondNumber);
-                break;
+                System.out.println("Result: " + (firstNumber + secondNumber));
+                 break;
             case "-":
-                System.out.println(firstNumber - secondNumber);
+                System.out.println("Result: " + (firstNumber - secondNumber));
                 break;
             case "*":
-                System.out.println(firstNumber * secondNumber);
+                System.out.println("Result: " + (firstNumber * secondNumber));
                 break;
             case "/":
-                System.out.println(firstNumber / secondNumber);
+                System.out.println("Result: " + (firstNumber / secondNumber));
                 break;
             default:
                 System.out.println("You entered an invalid operator!");
         }
-
+        } catch (ArithmeticException arithmeticException) {
+            System.out.println("Division by zero cannot be interpreted!");
+            System.out.println(arithmeticException.getMessage());
+        }
     }
 }
--- training-solutions/src/main/java/introexceptiontrycatch/FirstLetter.java
+++ training-solutions/src/main/java/introexceptiontrycatch/FirstLetter.java
@@ -7,14 +7,17 @@ import java.util.List;
 public class FirstLetter {
 
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>(Arrays.asList("répa", "mogyoró", "káposzta", "krumpli", "", null));
+        List<String> words = new ArrayList<>(Arrays.asList(null, "répa", "mogyoró", "", "káposzta", "krumpli", " "));
 
         for (String word : words) {
             try {
-                System.out.println(word.charAt(0));
-            } catch (StringIndexOutOfBoundsException stringIndexOutOfBoundsException) {
-                System.out.println("Empty string in the list!");
-                System.out.println(stringIndexOutOfBoundsException.getMessage());
+                word = word.trim();
+                try {
+                    System.out.println(word.charAt(0));
+                } catch (StringIndexOutOfBoundsException stringIndexOutOfBoundsException) {
+                    System.out.println("String contains only whitespaces, or empty!");
+                    System.out.println(stringIndexOutOfBoundsException.getMessage());
+                }
             } catch (NullPointerException npe) {
                 System.out.println("Null string in the list");
                 System.out.println(npe.getMessage());
