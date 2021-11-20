d9153e62f431042d532c0092e2ec5b5bb2a1cf7a
Attila <attila.ferenc.80@gmail.com>
2021-11-20 21:00:30 +0100

    introexceptioncause poem solved.

---
+++ training-solutions/src/main/java/introexceptioncause/Poem.java
@@ -0,0 +1,22 @@
+package introexceptioncause;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Poem {
+
+    public static void main(String[] args) {
+
+        try {
+            List<String> poem = Files.readAllLines(Paths.get("poem_.txt"));
+            for (String row : poem) {
+                System.out.print(row.charAt(0));
+            }
+            System.out.println();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't read file", ioe);
+        }
+    }
+}
