55034a449f18560620e74fdac6433eb90036cf26
Attila <attila.ferenc.80@gmail.com>
2021-11-20 19:33:28 +0100

    introexceptionreadfile solved.

---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,19 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+
+    public static void main(String[] args) {
+
+        try {
+            List<String> shoppingList = Files.readAllLines(Paths.get("shoppinglist.txt"));
+            System.out.println(shoppingList);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
