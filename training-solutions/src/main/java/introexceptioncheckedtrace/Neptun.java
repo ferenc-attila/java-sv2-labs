7fb322a963ae0f55fc4aee3aaf53192a62e3c08a
Attila <attila.ferenc.80@gmail.com>
2021-11-20 19:55:59 +0100

    introexceptioncheckedtrace Neptun solved.

---
+++ training-solutions/src/main/java/introexceptioncheckedtrace/Neptun.java
@@ -0,0 +1,35 @@
+package introexceptioncheckedtrace;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Neptun {
+
+    public static void main(String[] args) {
+
+        Neptun neptun = new Neptun();
+
+        List<String> contentOfFile;
+        try {
+            contentOfFile = neptun.readCsv("neptun.csv");
+            System.out.println(neptun.getNeptunCodes(contentOfFile));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private List<String> getNeptunCodes(List<String> contentOfFile) {
+        List<String> neptunCodes = new ArrayList<>();
+        for (String row : contentOfFile) {
+            neptunCodes.add(row.substring(row.indexOf(',') + 1));
+        }
+        return neptunCodes;
+    }
+
+    private List<String> readCsv(String filename) throws IOException {
+        return Files.readAllLines(Paths.get(filename));
+    }
+}
