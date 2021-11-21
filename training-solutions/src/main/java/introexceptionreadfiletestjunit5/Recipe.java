856996ed9c6666b93d7b15d35d3ea11fc86ee2e5
Attila <attila.ferenc.80@gmail.com>
2021-11-21 10:52:06 +0100

    introexceptionreadfiletestjunit5 Recipe code cleaning.

--- training-solutions/src/main/java/introexceptionreadfiletestjunit5/Recipe.java
+++ training-solutions/src/main/java/introexceptionreadfiletestjunit5/Recipe.java
@@ -3,7 +3,6 @@ package introexceptionreadfiletestjunit5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
