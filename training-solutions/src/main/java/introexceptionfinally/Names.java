cb7221b82e5692ace076273fdc1c82b13d9f5f1c
Attila <attila.ferenc.80@gmail.com>
2021-11-21 21:59:26 +0100

    introexceptionfinally names solved

--- training-solutions/src/main/java/introexceptionfinally/Names.java
+++ training-solutions/src/main/java/introexceptionfinally/Names.java
@@ -10,24 +10,46 @@ public class Names {
 
     public static void main(String[] args) {
 
+        Names names = new Names();
+
         Path namesWithSomeDoctors = Paths.get("src/main/resources/namesWithSomeDoctors.txt");
-        Path names = Paths.get("src/main/resources/names.txt");
+        Path simpleNames = Paths.get("src/main/resources/names.txt");
+        Path invalidPath = Paths.get("src/test/resources/names.txt");
 
+        try {
+            System.out.println(names.findFirstDoctor(namesWithSomeDoctors));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+            ise.getCause().printStackTrace();
+        }
 
+        try {
+            System.out.println(names.findFirstDoctor(simpleNames));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+            ise.getCause().printStackTrace();
+        }
+
+        try {
+            System.out.println(names.findFirstDoctor(invalidPath));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+            ise.getCause().printStackTrace();
+        }
     }
 
     private List<String> readFiles(Path path) {
         try {
             return Files.readAllLines(path);
         } catch (IOException ioe) {
-            throw new IllegalStateException("Unable to read file!");
+            throw new IllegalStateException("Unable to read file!", ioe);
         }
     }
 
     private String findFirstDoctor(Path path) {
         List<String> contentOfFile = readFiles(path);
         for (String row : contentOfFile) {
-            if ("dr.".equals(row.substring(0, 2))) {
+            if ("dr.".equals(row.substring(0, 3))) {
                 return row;
             }
         }
