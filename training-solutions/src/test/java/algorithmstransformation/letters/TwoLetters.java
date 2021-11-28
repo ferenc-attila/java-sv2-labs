a64c71e66dfd4f8fd15afe5cfb1e317db5dbb562
Attila <attila.ferenc.80@gmail.com>
2021-11-28 13:23:13 +0100

    algorithmstransformation.letters solved

---
+++ training-solutions/src/test/java/algorithmstransformation/letters/TwoLetters.java
@@ -0,0 +1,15 @@
+package algorithmstransformation.letters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoLetters {
+
+    public List<String> getFirstTwoLetters(List<String> words) {
+        List<String> firstTwoLetters = new ArrayList<>();
+        for (String word : words) {
+            firstTwoLetters.add(word.substring(0,2));
+        }
+        return firstTwoLetters;
+    }
+}
---
+++ training-solutions/src/test/java/algorithmstransformation/letters/TwoLettersTest.java
@@ -0,0 +1,25 @@
+package algorithmstransformation.letters;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoLettersTest {
+
+    @Test
+    void getFirstTwoLettersTest() {
+        TwoLetters twoLetters = new TwoLetters();
+
+        List<String> words = Arrays.asList("béka", "béna", "labda", "kérész", "békalencse", "Béla");
+        List<String> firstTwoLetters = twoLetters.getFirstTwoLetters(words);
+
+        assertEquals(6, firstTwoLetters.size());
+        assertEquals("bé", firstTwoLetters.get(0));
+        assertEquals("bé", firstTwoLetters.get(1));
+        assertEquals("ké", firstTwoLetters.get(3));
+        assertEquals("Bé", firstTwoLetters.get(5));
+    }
+}
