d6a88fc43654140520f354ca2321fd9022a9dfaf
Attila <attila.ferenc.80@gmail.com>
2021-12-01 21:08:39 +0100

    inheritanceconstructor Animnal solved

---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/Sparrow.java
@@ -0,0 +1,10 @@
+package inheritanceconstructor.animal;
+
+public class Sparrow extends Bird {
+
+    public Sparrow() {
+        super();
+        System.out.println("Sparrow class constructor");
+    }
+}
+
