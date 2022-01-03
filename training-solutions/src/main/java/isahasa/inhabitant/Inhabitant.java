b7cfeb6dcb3405355963b8aa5060ea0b0d653711
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-03 13:32:01 +0100

    isahasa inhabitant solved

---
+++ training-solutions/src/main/java/isahasa/inhabitant/Inhabitant.java
@@ -0,0 +1,19 @@
+package isahasa.inhabitant;
+
+public class Inhabitant extends Person {
+
+    private ParkingPlace parkingPlace;
+
+    public Inhabitant(String name, ParkingPlace parkingPlace) {
+        super(name);
+        this.parkingPlace = parkingPlace;
+    }
+
+    public void changeParkingPlace (int newNumber) {
+        this.parkingPlace = new ParkingPlace(newNumber);
+    }
+
+    public ParkingPlace getParkingPlace() {
+        return parkingPlace;
+    }
+}
