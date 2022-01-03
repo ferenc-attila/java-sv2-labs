ca84e051b3e366956ad74b9da3ce19c623f3d5c4
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-04 00:03:21 +0100

    isahasa fleet updated

--- training-solutions/src/main/java/isahasa/fleet/Fleet.java
+++ training-solutions/src/main/java/isahasa/fleet/Fleet.java
@@ -9,30 +9,44 @@ public class Fleet {
     private int waitingPeople;
     private int waitingCargo;
 
-    public void addShip (Ship ship) {
+    public void addShip(Ship ship) {
         ships.add(ship);
     }
 
-    public void loadShip (int passengers, int cargoWeight) {
+    public void loadShip(int passengers, int cargoWeight) {
+        int remainingPersons = passengers;
+        int remainingCargo = cargoWeight;
         for (Ship ship : ships) {
-            if (ship.getClass() == Liner.class) {
-                passengers = ((Liner) ship).loadPassenger(passengers);
-            } else if (ship.getClass() == CargoShip.class) {
-                cargoWeight = ((CargoShip) ship).loadCargo(cargoWeight);
-            } else if (ship.getClass() == FerryBoat.class) {
-                cargoWeight = ((FerryBoat) ship).loadCargo(cargoWeight);
-                passengers = ((FerryBoat) ship).loadPassenger(passengers);
+            if (ship instanceof CanCarryPassengers) {
+                remainingPersons = getRemainingPersons(remainingPersons, (CanCarryPassengers) ship);
             }
+            if (ship instanceof CanCarryGoods) {
+                remainingCargo = getRemainingCargo(remainingCargo, (CanCarryGoods) ship);
+            }
+            this.waitingPeople = remainingPersons;
+            this.waitingCargo = remainingCargo;
         }
-        this.waitingPeople = passengers;
-        this.waitingCargo = cargoWeight;
     }
 
-    public int getWaitingPeople() {
-        return waitingPeople;
+    private int getRemainingCargo(int remainingCargo, CanCarryGoods ship) {
+        if (remainingCargo != 0) {
+            remainingCargo = ship.loadCargo(remainingCargo);
+        }
+        return remainingCargo;
+    }
+
+    private int getRemainingPersons(int remainingPersons, CanCarryPassengers ship) {
+        if (remainingPersons != 0) {
+            remainingPersons = ship.loadPassenger(remainingPersons);
+        }
+        return remainingPersons;
     }
 
     public int getWaitingCargo() {
         return waitingCargo;
+    }
+
+    public int getWaitingPeople() {
+        return waitingPeople;
     }
 }
