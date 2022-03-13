6384d41bdc3321bf6f5af1c3319b0e1d8a4509d0
ferenc-attila <attila.ferenc.80@gmail.com>
2022-03-13 08:20:26 +0100

    added README.md from task description readExcel implemented Client with validations implemented

--- CovidVaccination/src/main/java/readtable/excel/ReadExcelXml.java
+++ CovidVaccination/src/main/java/readtable/excel/ReadExcelXml.java
@@ -1,11 +1,17 @@
 package readtable.excel;
 
+import registration.Client;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ReadExcelXml {
 
@@ -22,12 +28,35 @@ public class ReadExcelXml {
         }
     }
 
-    public void readFile() {
+    public List<Client> readFile() {
         try (FileInputStream inputStream = new FileInputStream(file)) {
             Workbook workbook = new XSSFWorkbook(inputStream);
             Sheet sheet = workbook.getSheetAt(0);
+            return readSheet(sheet);
         } catch (IOException ioe) {
-            throw new IllegalArgumentException("Cannot read file: " + file);
+            throw new IllegalArgumentException("Cannot read file: " + file + "!", ioe);
+        }
+    }
+
+    private List<Client> readSheet(Sheet sheet) {
+        List<Client> clients = new ArrayList<>();
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        while (rowIterator.hasNext()) {
+            clients.add(createClients(rowIterator.next()));
+        }
+        return clients;
+    }
+
+    private Client createClients(Row row) {
+        Iterator<Cell> cellIterator = row.cellIterator();
+        try {
+            return new Client(cellIterator.next().getStringCellValue(),
+                    cellIterator.next().getStringCellValue(),
+                    Integer.parseInt(cellIterator.next().getStringCellValue()),
+                    cellIterator.next().getStringCellValue(),
+                    cellIterator.next().getStringCellValue());
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Invalid age value in the table!", nfe);
         }
     }
 
---
+++ CovidVaccination/src/main/java/registration/Client.java
@@ -0,0 +1,143 @@
+package registration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    private static final int MIN_AGE = 11;
+    private static final int MAX_AGE = 149;
+    private static final int LENGTH_OF_SOCIAL_SECURITY_NUMBER = 9;
+
+    private String name;
+    private String postalCode;
+    private int age;
+    private String email;
+    private String socialSecurityNumber;
+
+    public Client(String name, String postalCode, int age, String email, String socialSecurityNumber) {
+        validateParameters(name, postalCode, age, email, socialSecurityNumber);
+        this.name = name;
+        this.postalCode = postalCode;
+        this.age = age;
+        this.email = email;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    private void validateParameters(String name, String postalCode, int age, String eMail, String socialSecurityNumber) {
+        validateName(name);
+        validatePostalCode(postalCode);
+        validateAge(age);
+        validateEMail(eMail);
+        validateSocialSecurityNumber(socialSecurityNumber);
+    }
+
+    private void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be empty!");
+        }
+    }
+
+    private String validatePostalCode(String postalCode) {
+        if (postalCode == null) {
+            throw new IllegalArgumentException("Postal code cannot be empty!");
+        }
+        // Get list of municipalities from table and return name of municipality!
+        // If not in the list, throw new exception!
+        return "town";
+    }
+
+    private void validateAge(int age) {
+        if (age < MIN_AGE || age > MAX_AGE) {
+            throw new IllegalArgumentException("Age must be between " + MIN_AGE + " and " + MAX_AGE + "!");
+        }
+    }
+
+    private void validateEMail(String eMail) {
+        if (eMail == null || eMail.length() < 5 || eMail.indexOf('@') < 1 || eMail.indexOf('.') < 3) {
+            throw new IllegalArgumentException("Invalid e-mail address: " + eMail + "!");
+        }
+    }
+
+    private void validateSocialSecurityNumber(String socialSecurityNumber) {
+        if (socialSecurityNumber == null || socialSecurityNumber.length() != LENGTH_OF_SOCIAL_SECURITY_NUMBER) {
+            throw new IllegalArgumentException("Social security number must consist of 10 numeric characters!");
+        }
+        validateCdvCode(socialSecurityNumber);
+    }
+
+    private void validateCdvCode(String socialSecurityNumber) {
+        List<Integer> valuesOfSocialSecurityNumber = getValuesOfSocialSecurityNumber(socialSecurityNumber);
+        int cdvCode = getCdvCode(valuesOfSocialSecurityNumber);
+        if (cdvCode != valuesOfSocialSecurityNumber.get(LENGTH_OF_SOCIAL_SECURITY_NUMBER - 1)) {
+            throw new IllegalArgumentException("Invalid CDV code in social security number!");
+        }
+    }
+
+    private int getCdvCode(List<Integer> valuesOfSocialSecurityNumber) {
+        int cdvCode = 0;
+        for (int i = 0; i < LENGTH_OF_SOCIAL_SECURITY_NUMBER; i++) {
+            if (i < 8 && i % 2 == 0) {
+                cdvCode += valuesOfSocialSecurityNumber.get(i) * 3;
+            }
+            if (i < 8 && i % 2 == 1) {
+                cdvCode += valuesOfSocialSecurityNumber.get(i) * 7;
+            }
+        }
+        cdvCode = cdvCode / 10;
+        return cdvCode;
+    }
+
+    private List<Integer> getValuesOfSocialSecurityNumber(String socialSecurityNumber) {
+        List<Integer> valuesOfSocialSecurityNumber = new ArrayList<>();
+        char[] chars = socialSecurityNumber.toCharArray();
+        try {
+            for (char c : chars) {
+                valuesOfSocialSecurityNumber.add((int) c);
+            }
+        } catch (ClassCastException cce) {
+            throw new IllegalArgumentException("Invalid character in social security number!", cce);
+        }
+        return valuesOfSocialSecurityNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+}
