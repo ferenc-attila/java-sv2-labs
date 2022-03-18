051011b6e5ea2ee89937fd08ef17a3e0445a72a2
ferenc-attila <attila.ferenc.80@gmail.com>
2022-03-18 06:54:26 +0100

    backgruond data (postal codes, vaccina types) csv files changed to sql

---
+++ CovidVaccination/src/main/resources/db/migration/V3__vaccina_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO vaccination_test.vaccina_types (name,primary_series,min_age,max_age) VALUES
+	 ('Pfizer',2,5,149),
+	 ('Astra Zeneca',2,18,149),
+	 ('Janssen',1,18,149),
+	 ('Moderna',2,18,149);
